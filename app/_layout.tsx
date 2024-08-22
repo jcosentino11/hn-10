d6b7c6d53baa1dd7ee6b7ae4bab2075c7a8de740
Joseph Cosentino <jcosentino11@pm.me>
2024-08-21 20:38:13 -0700

    make stack react to color scheme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   return (
-    <Stack>
-      <Stack.Screen 
-        name="index"
-        options={{ title: 'HN Top 10' }} />
-    </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "HN Top 10" }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
