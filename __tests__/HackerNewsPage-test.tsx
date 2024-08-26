ad03e49a2c55602161c5df34785714b27706b394
Joseph Cosentino <jcosentino11@pm.me>
2024-08-25 18:57:31 -0700

    refactor test for dynamic num of stories

--- __tests__/HackerNewsPage-test.tsx
+++ __tests__/HackerNewsPage-test.tsx
@@ -1,21 +1,30 @@
 import HackerNewsPage from '@/components/HackerNewsPage';
 import { render, screen, waitFor } from '@testing-library/react-native';
 
-const clientMock = {
-  fetchHackerNewsStories: jest.fn(() =>
-    Promise.resolve([
-      { objectID: '1', title: 'Story 1', url: 'https://example.com/1', author: 'Author 1' },
-      { objectID: '2', title: 'Story 2', url: 'https://example.com/2', author: 'Author 2' },
-    ])
-  ),
+const generateStories = (numStories: number) => 
+    Array.from({ length: numStories }, (_, index) => ({
+      objectID: (index + 1).toString(),
+      title: `Story ${index + 1}`,
+      url: `https://example.com/${index + 1}`,
+      author: `Author ${index + 1}`
+    }));
+
+const createClientMock = (numResults: number) => {
+  return {
+    fetchHackerNewsStories: jest.fn(() =>
+      Promise.resolve(generateStories(numResults))
+    ),
+  };
 };
 
 describe('<HackerNewsPage />', () => {
   test('Stories render after being fetched', async () => {
-    render(<HackerNewsPage numberOfStories={2} onDataFetched={() => {}} client={clientMock} />);
+    const numStories = 10;
+    render(<HackerNewsPage numberOfStories={2} onDataFetched={() => {}} client={createClientMock(numStories)} />);
     await waitFor(() => {
-      expect(screen.getByText('Story 1')).toBeTruthy();
-      expect(screen.getByText('Story 2')).toBeTruthy();
+      for (let i = 1; i <= numStories; i++) {
+        expect(screen.getByText(`Story ${i}`)).toBeTruthy();
+      }
     });
   });
 });
